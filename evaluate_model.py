5876ff96eebae2a8c256d1c31f2391c58cb3b9a2
senthil <sguna@stanford.edu>
2019-05-27 23:07:59 +0000

    added precision recall and confusion matrix

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,6 +1,8 @@
 import json
 import numpy as np
 import tensorflow as tf 
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 from tensorflow.keras.utils import plot_model
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.models import Model
@@ -12,14 +14,11 @@ from tensorflow.keras.optimizers import Nadam
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 from tensorflow.keras.utils import multi_gpu_model
 from keras.models import load_model
-
 from CNN_LSTM_load_data import  generator_train, generator_test, generator_eval
 from CNN_LSTM_split_data import generate_feature_train_list, generate_feature_test_list, generate_feature_eval_list
-#from utils.CustomImageCallback import CustomImageCallback
 
 config = json.load(open('config/config.json'))
 base_dir = config['base_dir']
-model_save_dir = config["model_save_dir"]
 history_dir = config["history_dir"]
 
 base_image_dir = base_dir+"images/"
@@ -36,8 +35,8 @@ print(train_image_dir, train_label_dir) # Senthil Remove
 print(eval_image_dir, eval_label_dir) # Senthil Remove
 
 # 7 phases for surgical operation
-class_labels = {"Preparation":0, "CalotTriangleDissection":1, "ClippingCutting":2, 
-           "GallbladderDissection":3, "GallbladderPackaging":4, "CleaningCoagulation":5, "GallbladderRetraction":6}
+class_labels = ["Preparation", "CalotTriangleDissection", "ClippingCutting", 
+           "GallbladderDissection", "GallbladderPackaging", "CleaningCoagulation", "GallbladderRetraction"]
 
 
 num_classes = 7
@@ -71,26 +70,60 @@ validation_len = (validation_len-2)*BATCH_SIZE*frames
 validation_samples = validation_samples[0:validation_len]
 print ("Validatation Length:", validation_len)
 
-# load training data
-validation_generator = generator_eval(validation_samples, batch_size=BATCH_SIZE, frames_per_clip=frames, shuffle=False)
-
 #define callback functions
 #OuputImageCallback = CustomImageCallback( validation_generator, log_dir = './logs/Graph')
 
 model_callbacks = [TensorBoard(log_dir='./logs/Graph', histogram_freq=5, write_graph=True, write_images=True, write_grads=True)]
-
-
-
-#loss = lstm_model.evaluate_generator(validation_generator, steps=int(len(validation_samples)/(BATCH_SIZE*frames)), max_queue_size=10, workers=1, use_multiprocessing=False, verbose=1)
-pred = lstm_model.predict_generator(validation_generator, steps=int(len(validation_samples)/(BATCH_SIZE*frames)), max_queue_size=10, workers=1, use_multiprocessing=False, verbose=1)
 lstm_model.summary()
 
+# load data and predict
+y = None
+yhat = None
+num_batches = int(len(validation_samples)/(BATCH_SIZE*frames))
+num_batches = 2 # test code delete later
+validation_generator = generator_eval(validation_samples, batch_size=BATCH_SIZE, frames_per_clip=frames, shuffle=False)
+steps = 0
+for x_batch, y_batch in validation_generator:
+    steps+=1
+    print("Input shape {0}, X-Length {1}, gt shape {2}, Y-Length {3}".format(np.shape(x_batch),len(x_batch[0]), np.shape(y_batch), len(y_batch)))
+    if y is None:
+        y = y_batch
+    else: 
+        y = np.concatenate((y, y_batch))
+
+    # loss = lstm_model.evaluate_generator(validation_generator, steps=int(len(validation_samples)/(BATCH_SIZE*frames)), max_queue_size=10, workers=1, use_multiprocessing=False, verbose=1)
+    pred_batch = lstm_model.predict(x = x_batch, verbose=1)
+    print(np.shape(pred_batch)) 
+    if yhat is None:
+        yhat = pred_batch
+    else:
+        yhat = np.concatenate((yhat, pred_batch))
+    #yhat.append(pred_batch)
+    if steps >= num_batches: break
+
+print("yhat before",np.shape(yhat), yhat)
+yhat = np.argmax(yhat, axis=1)
+print("yhat after", np.shape(yhat), yhat)
+
+y =  np.argmax(y, axis=1)
+
 predfile = open('./logs/predictions_Yhat.txt', 'wt')
-predfile.write('\r\n'.join(str(p).strip() for p in pred))
+predfile.write('\r\n'.join(str(p).strip() for p in yhat))
 predfile.close()
 
 validationfile = open('./logs/input_X.txt', 'wt')
 validationfile.write('\r\n'.join(str(s) for s in validation_samples))
 validationfile.close()
 
-print("predictions", pred)
+gtfile = open('./logs/gt_Y.txt', 'wt')
+gtfile.write('\r\n'.join(str(s) for s in y))
+gtfile.close()
+print("ground truth", [class_labels[i] for i in y])
+print("predictions", [class_labels[i] for i in yhat])
+
+cm = confusion_matrix(y, yhat, labels = [0, 1, 2, 3, 4, 5, 6])
+print(cm)
+
+cr = classification_report(y, yhat, [0, 1, 2, 3, 4, 5, 6], class_labels)
+print(cr)
+
