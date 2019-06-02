bc1fe27c891e6dff2d3fe7b98d28e95fc8db2cb4
madhuhegde <madhu.hegde@gmail.com>
2019-06-02 14:13:12 -0700

    stateful change

--- CNN_SF_LSTM_Train_tf.py
+++ CNN_SF_LSTM_Train_tf.py
@@ -173,14 +173,14 @@ callbacks = [EarlyStopping(monitor='val_loss', patience=3, verbose=2),
  
 def train_stateful_lstm(lstm_model, train_videos, test_videos, callbacks):
 
-  train_samples  = generate_feature_train_list(train_image_dir, train_label_dir, train_videos)
+  train_samples  = generate_feature_train_list(train_image_dir, train_label_dir, ['video02', 'video17', 'video'25']) #train_videos)
   
   train_samples = remove_transition_samples(train_samples, frames)
   print(len(train_samples))
   #class_weights = compute_class_weight(train_samples)
   
 
-  validation_samples = generate_feature_test_list(test_image_dir, test_label_dir, test_videos)
+  validation_samples = generate_feature_test_list(test_image_dir, test_label_dir, ['video04', 'video21','video24']) #test_videos)
   
   validation_samples = remove_transition_samples(validation_samples, frames)
   
