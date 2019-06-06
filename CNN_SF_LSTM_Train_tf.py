29485ed99b77c60807a632ec5e602d68b5b0a711
madhuhegde <madhu.hegde@gmail.com>
2019-06-06 01:37:44 -0700

    debug code

--- CNN_SF_LSTM_Train_tf.py
+++ CNN_SF_LSTM_Train_tf.py
@@ -55,10 +55,8 @@ rows = 224
 columns = 224 
 
 #training parameters
-BATCH_SIZE = 8# Need GPU with 32 GB RAM for BATCH_SIZE > 16
-nb_epochs = 3 # 
-
-class_weights = [2,2,1,2,2,1,1]
+BATCH_SIZE = 8 # Need GPU with 32 GB RAM for BATCH_SIZE > 16
+nb_epochs = 2 # 
 
 
 # Define callback function if detailed log required
@@ -94,134 +92,121 @@ class CNN_LSTM_ModelCheckpoint(tensorflow.keras.callbacks.Callback):
         val_acc = logs.get('val_categorical_accuracy')
         if(val_acc > self.max_val_acc):
            self.max_val_acc = val_acc
-           #self.cnn_model.save(self.cnn_filename) 
+           self.cnn_model.save(self.cnn_filename) 
            self.lstm_model.save(self.lstm_filename)
           
- 
- 
-def train_stateful_lstm(lstm_model, train_videos, test_videos, callbacks):
 
-  print(train_videos)
-  train_samples  = generate_feature_train_list(train_image_dir, train_label_dir, train_videos)
-  
-  train_samples = remove_transition_samples(train_samples, frames)
-  print(len(train_samples))
-  #class_weights = compute_class_weight(train_samples)
-  
+#Define Input with batch_shape to train stateful LSTM  
+video = Input(batch_shape=(BATCH_SIZE, frames,rows,columns,channels))
 
-  validation_samples = generate_feature_test_list(test_image_dir, test_label_dir, test_videos)
-  
-  validation_samples = remove_transition_samples(validation_samples, frames)
-  
-  train_len = int(len(train_samples)/(BATCH_SIZE*frames))
-  train_len = (train_len)*BATCH_SIZE*frames
-  train_samples = train_samples[0:train_len]
-  validation_len = int(len(validation_samples)/(BATCH_SIZE*frames))
-  validation_len = (validation_len-2)*BATCH_SIZE*frames
-  validation_samples = validation_samples[0:validation_len]
-  print (train_len, validation_len)
+#load lstm_model with shuffled data
+prev_lstm_model = load_model(model_save_dir+'lstm_model.h5')
+lstm_weights = list()
 
+#load pretrained weights
+for layer in prev_lstm_model.layers:
+    weights = layer.get_weights()
+    lstm_weights.append(weights)
 
+# print summary and clear model    
+prev_lstm_model.summary
+del prev_lstm_model
+
+#load pre-trained cnn model
+cnn_model = load_model(model_save_dir+'cnn_model.h5')
+
+#freeze cnn weights for stateful LSTM
+for layer in cnn_model.layers:
+   layer.trainable = False
 
 
-  # load training data
-  train_generator = generator_train(train_samples, batch_size=BATCH_SIZE, frames_per_clip=frames,shuffle=False)
-  validation_generator = generator_test(validation_samples, batch_size=BATCH_SIZE, frames_per_clip=frames, shuffle=False)
 
-  lstm_model.fit_generator(train_generator, 
-            steps_per_epoch=int(len(train_samples)/(BATCH_SIZE*frames)), 
-            validation_data=validation_generator, 
-            validation_steps=int(len(validation_samples)/(BATCH_SIZE*frames)), 
-            class_weight = class_weights,
-            callbacks = callbacks,
-            epochs=nb_epochs, verbose=1)
-  
-  
-  return    
+#cnn_model.summary()
 
+#pdb.set_trace()
 
-if __name__=="__main__":
-  
-  #Define Input with batch_shape to train stateful LSTM  
-  video = Input(batch_shape=(BATCH_SIZE, frames,rows,columns,channels))
+encoded_frames = TimeDistributed(cnn_model)(video)
+encoded_sequence = LSTM(2048, stateful=True, name='lstm1')(encoded_frames)
 
-  #load lstm_model with shuffled data
-  prev_lstm_model = load_model(model_save_dir+'lstm_model.h5')
-  lstm_weights = list()
+# RELU or tanh?
+hidden_layer = Dense(units=2048, activation="relu")(encoded_sequence)
+#hidden_layer = Dense(units=512, activation="tanh")(encoded_sequence)
 
-  #load pretrained weights
-  for layer in prev_lstm_model.layers:
-     weights = layer.get_weights()
-     lstm_weights.append(weights)
+dropout_layer = Dropout(rate=0.5)(hidden_layer)
+outputs = Dense(units=num_classes, activation="softmax")(dropout_layer)
+lstm_model = Model(video, outputs)
 
-  # print summary and clear model    
-  prev_lstm_model.summary
-  del prev_lstm_model
-
-  #load pre-trained cnn model
-  cnn_model = load_model(model_save_dir+'cnn_model.h5')
-
-  #freeze cnn weights for stateful LSTM
-  for layer in cnn_model.layers:
-    layer.trainable = False
-
-  #LSTM model must match stateless LSTM
-  encoded_frames = TimeDistributed(cnn_model)(video)
-  encoded_sequence = LSTM(2048, stateful=True, name='lstm1')(encoded_frames)
-
-  # RELU or tanh?
-  hidden_layer = Dense(units=2048, activation="relu")(encoded_sequence)
-
-
-  dropout_layer = Dropout(rate=0.5)(hidden_layer)
-  outputs = Dense(units=num_classes, activation="softmax")(dropout_layer)
-  lstm_model = Model(video, outputs)
-
-  for i in range(len(lstm_model.layers)):
+for i in range(len(lstm_model.layers)):
     lstm_model.layers[i].set_weights(lstm_weights[i])
 
 
-  #Similar to Adam 
-  optimizer = Nadam(lr=0.00005,
+#Similar to Adam
+optimizer = Nadam(lr=0.00001,
                   beta_1=0.9,
                   beta_2=0.999,
                   epsilon=1e-08,
                   schedule_decay=0.004)
 
-  #softmax crossentropy
-  lstm_model.compile(loss="categorical_crossentropy",
+#softmax crossentropy
+lstm_model.compile(loss="categorical_crossentropy",
               optimizer=optimizer,
               metrics=["categorical_accuracy"]) 
 
+
+
+#generate indices for train_array an test_array with train_test_split_ratio = 0.
+
+
+train_samples  = generate_feature_train_list(train_image_dir, train_label_dir, train_videos)
+validation_samples = generate_feature_test_list(test_image_dir, test_label_dir, test_videos)
+train_len = int(len(train_samples)/(BATCH_SIZE*frames))
+train_len = (train_len)*BATCH_SIZE*frames
+train_samples = train_samples[0:train_len]
+validation_len = int(len(validation_samples)/(BATCH_SIZE*frames))
+validation_len = (validation_len-2)*BATCH_SIZE*frames
+validation_samples = validation_samples[0:validation_len]
+print (train_len, validation_len)
+
+saveCNN_Model = CNN_LSTM_ModelCheckpoint(cnn_model, model_save_dir+"cnn_model.h5",
+                                    lstm_model, model_save_dir+"lstm_model.h5")
+
 #define callback functions
-  
-  saveCNN_Model = CNN_LSTM_ModelCheckpoint(cnn_model, model_save_dir+"cnn_model_notsaved.h5",
-                                    lstm_model, model_save_dir+"lstm_stateful_model.h5")
-  history = History()
-  callbacks = [EarlyStopping(monitor='val_loss', patience=3, verbose=2),
-               #ModelCheckpoint(filepath=model_save_dir+'best_model.h5', monitor='val_loss',
-               #save_best_only=True),
-               history,
-               saveCNN_Model]
- #             TensorBoard(log_dir='./logs/Graph', histogram_freq=0, write_graph=True, write_images=True)]
- 
-  
+callbacks = [EarlyStopping(monitor='val_loss', patience=3, verbose=2),
+             #ModelCheckpoint(filepath=model_save_dir+'best_model.h5', monitor='val_loss',
+             #save_best_only=True),
+             saveCNN_Model]
+ #            TensorBoard(log_dir='./logs/Graph', histogram_freq=0, write_graph=True, write_images=True)]
 
-  train_stateful_lstm(lstm_model, train_videos, test_videos, callbacks)
+# load training data
+train_generator = generator_train(train_samples, batch_size=BATCH_SIZE, frames_per_clip=frames,shuffle=False)
+validation_generator = generator_test(validation_samples, batch_size=BATCH_SIZE, frames_per_clip=frames, shuffle=False)
 
+history = lstm_model.fit_generator(train_generator, 
+            steps_per_epoch=int(len(train_samples)/(BATCH_SIZE*frames)), 
+            validation_data=validation_generator, 
+            validation_steps=int(len(validation_samples)/(BATCH_SIZE*frames)), 
+            #callbacks = [history],
+            #callbacks = callbacks,
+            epochs=nb_epochs, verbose=1)
+
+#plot_model(model, to_file='./logs/model.png', show_shapes=True)
+#logfile = open('./logs/losses.txt', 'wt')
+#logfile.write('\n'.join(str(l) for l in history.history['loss']))
+#logfile.close()
+                        
 #history.key() = ['loss', 'categorical_accuracy', 'val_loss', 'val_categorical_accuracy'])
+print(history.history['loss'])
 
-  history_dict = {}
-  history_dict['val_loss'] = history.val_loss
-  history_dict['train_loss'] = history.train_loss
-  history_dict['train_acc'] = history.train_acc
-  history_dict['val_acc'] = history.val_acc
-
-  with open(history_dir+'sf_lstm_model_history', 'wb') as file_pi:
-        pickle.dump(history_dict, file_pi)
+#dump history
+#json.dump(history.history, open(history_dir+'model_history', 'w'))
+with open(history_dir+'lstm_model_history', 'wb') as file_pi:
+        pickle.dump(history.history, file_pi)
         
-#save model and clear session
-  del lstm_model, cnn_model
-  tensorflow.keras.backend.clear_session()
+#print(history.val_acc)
+#plt.title('model accuracy')
+#plt.ylabel('accuracy')
+#plt.show()
 
-  
+#save model and clear session
+del lstm_model
+tensorflow.keras.backend.clear_session()
